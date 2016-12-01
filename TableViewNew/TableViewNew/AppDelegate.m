b0cf122ed31e50fc377e1d68c9f0ededbc8bb961
hzw <hanzhiwei018@qingan.com>
2016-12-01 17:26:50 +0800

    APP进入后台添加毛玻璃效果

--- TableViewNew/TableViewNew/AppDelegate.m
+++ TableViewNew/TableViewNew/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 #import "HSViewConrtroller.h"
+#import "BackGroundView.h"
 
 @interface AppDelegate ()
 
@@ -38,6 +39,8 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    /**< APP进入后台添加模糊图片 */
+    [BackGroundView showBackGroundView];
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -52,6 +55,8 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    /**< APP从后台唤醒移除模糊图片 */
+    [BackGroundView removeBackGroundView];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
