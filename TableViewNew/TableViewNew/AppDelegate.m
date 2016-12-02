ec51ae07a4ccf51cd3a175205766ccdf977743e3
hzw <hanzhiwei018@qingan.com>
2016-12-02 13:40:45 +0800

    将要进入后台 -- 进入后台

--- TableViewNew/TableViewNew/AppDelegate.m
+++ TableViewNew/TableViewNew/AppDelegate.m
@@ -39,13 +39,14 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    /**< APP进入后台添加模糊图片 */
-    [BackGroundView showBackGroundView];
+    /**< 将要进入后台 */
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    /**< APP进入后台添加模糊图片 */
+    [BackGroundView showBackGroundView];
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
