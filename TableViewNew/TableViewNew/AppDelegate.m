44c00ed4af7eb4a99c080699858cb12faf3666f2
hzw <hanzhiwei018@qingan.com>
2017-10-19 16:07:49 +0800

    [Utility]  添加一个全屏的吸附屏幕边缘的按钮

--- TableViewNew/TableViewNew/AppDelegate.m
+++ TableViewNew/TableViewNew/AppDelegate.m
@@ -11,6 +11,8 @@
 #import "HSViewConrtroller.h"
 #import "BackGroundView.h"
 
+#define kWidth     60
+
 @interface AppDelegate ()
 
 @end
@@ -29,8 +31,56 @@
     
     [self.window makeKeyAndVisible];
     
+    /**< 添加一个全屏的吸附屏幕边缘的按钮 */
+    UIButton *circleBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    circleBtn.frame = CGRectMake(100, 100, kWidth, kWidth);
+    circleBtn.layer.cornerRadius = kWidth/2;
+    circleBtn.backgroundColor = [UIColor blueColor];
+    [self.window.rootViewController.view addSubview:circleBtn];
+    
+    UIPanGestureRecognizer *pangGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
+    [circleBtn addGestureRecognizer:pangGesture];
+    
     return YES;
 }
+
+- (void)panGesture:(UIPanGestureRecognizer *)panGesture
+{
+    CGPoint translation = [panGesture translationInView:self.window.rootViewController.view];
+    CGFloat centerX = translation.x + panGesture.view.center.x;
+    CGFloat centerY = translation.y + panGesture.view.center.y;
+    CGFloat adjustX = 0;
+    CGFloat adjustY = 0;
+
+    panGesture.view.center = CGPointMake(centerX, centerY);
+    [panGesture setTranslation:CGPointZero inView:self.window.rootViewController.view];
+
+    if (panGesture.state == UIGestureRecognizerStateEnded ||
+        panGesture.state == UIGestureRecognizerStateCancelled)
+    {
+        /**< 计算偏移x坐标 */
+        if (centerX > kMainScreenWidth/2) {
+            adjustX = kMainScreenWidth - kWidth/2;
+        }else{
+            adjustX = kWidth/2;
+        }
+        /**< 计算偏移y坐标 */
+        if (centerY < kWidth/2) {
+            adjustY = kWidth/2;
+        }else if (centerY > kMainScreenHeight - kWidth/2){
+            adjustY = kMainScreenHeight - kWidth/2;
+        }else{
+            adjustY = centerY;
+        }
+
+        [UIView animateWithDuration:0.5 animations:^{
+            panGesture.view.center = CGPointMake(adjustX, adjustY);
+        }];
+    }
+}
+
+
+
 
 #pragma mark  禁止横屏(实现方法)
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
