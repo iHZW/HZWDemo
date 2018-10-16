decea5feb182ca0978fb7f488c7b2efdd4ef6e74
hzw <hanzhiwei018@qingan.com>
2018-10-16 10:45:51 +0800

    [Utility] 添加新建静态库文件的地址

--- TableViewNew/TableViewNew/MyFirst/include/MyFirstA.h
+++ TableViewNew/TableViewNew/MyFirst/include/MyFirstA.h
@@ -6,7 +6,13 @@
 //  Copyright © 2018年 HZW. All rights reserved.
 //
 
+/**<
+ https://www.jianshu.com/p/ca2f329254e8
+ 新建静态库文件
+ */
+
 #import <Foundation/Foundation.h>
+
 
 @interface MyFirstA : NSObject
 
